935bb2aa402b2c98eb5152f3cfd4c0c98ebe7500
Josh Hall <74386740+joshdhall@users.noreply.github.com>
2025-06-18 12:04:00 -0400

    Update setup_guide.sql

--- cortex-hol/setup_guide.sql
+++ cortex-hol/setup_guide.sql
@@ -100,6 +100,14 @@ CREATE OR REPLACE TABLE cortex_hol.raw_pos.customers
     phone_number VARCHAR(16777216)
 );
 
+-- review table build
+CREATE OR REPLACE TABLE cortex_hol.raw_pos.reviews (
+    REVIEW_ID STRING, 
+    CUSTOMER_ID INT,
+    STAR_RATING INT,
+    REVIEW_TEXT STRING,
+    REVIEW_DATE DATE
+);
 
 
 -- menu table load
@@ -117,6 +125,9 @@ FROM @cortex_hol.raw_pos.s3load/order-detail/;
 -- reviews table load
 COPY INTO cortex_hol.raw_pos.customers
 FROM @cortex_hol.raw_pos.s3load/customer/;
+
+COPY INTO cortex_hol.raw_pos.reviews
+FROM @cortex_hol.raw_pos.s3load/reviews/;
 
 
 -- remove xlarge warehouse 
